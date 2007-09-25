bb76f34628764452ff29fa90c33c9772fe527d3a
Kevin Ansfield <kev@digitalblueprint.co.uk>
2007-09-25 14:59:47 +0000

    Fix news observer/mailer to operate in the correct way to send multiple emails
    git-svn-id: svn://svn.digitalblueprint.co.uk/viper/trunk@69 31e3d8c1-9a35-0410-80f7-bb7063ef511c

--- app/models/news_mailer.rb
+++ app/models/news_mailer.rb
@@ -2,16 +2,13 @@ class NewsMailer < ActionMailer::ARMailer
   
   helper :application
   
-  def posted_news(news)
-    users = User.find_all_for_news_delivery
-    users.each do |user|
-      @recipients  = user.email
-      @from        = "\"#{SITENAME}\" <#{VIPER_EMAIL}>"
-      @subject     = "#{SITENAME} - #{news.title}"
-      @sent_on     = Time.now
-      @body[:user] = user
-      @body[:news] = news
-    end
+  def posted_news(news, user)
+    @recipients  = user.email
+    @from        = "\"#{SITENAME}\" <#{VIPER_EMAIL}>"
+    @subject     = "#{SITENAME} - #{news.title}"
+    @sent_on     = Time.now
+    @body[:user] = user
+    @body[:news] = news
   end
   
 end
--- app/models/news_observer.rb
+++ app/models/news_observer.rb
@@ -1,7 +1,12 @@
 class NewsObserver < ActiveRecord::Observer
   
   def after_create(news)
-    NewsMailer.deliver_posted_news(news) if news.send_as_email?
+    if news.send_as_email?
+      users = User.find_all_for_news_delivery
+      users.each do |user|
+        NewsMailer.deliver_posted_news(news, user)
+      end
+    end
   end
 
   def after_save(news)
