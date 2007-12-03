d6afac06753067f6b86f4d903fe0bacf19ae3b9e
Kevin Ansfield <kev@digitalblueprint.co.uk>
2007-12-03 15:31:04 +0000

    Make sure wall comments appear newest at the top
    git-svn-id: svn://svn.digitalblueprint.co.uk/viper/trunk@158 31e3d8c1-9a35-0410-80f7-bb7063ef511c

--- app/models/wall.rb
+++ app/models/wall.rb
@@ -1,4 +1,4 @@
 class Wall < ActiveRecord::Base
   belongs_to :user
-  has_many :comments, :as => :commentable, :order => 'created_at', :dependent => :destroy
+  has_many :comments, :as => :commentable, :order => 'created_at DESC', :dependent => :destroy
 end
