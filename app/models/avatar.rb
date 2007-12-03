dc46ab2891be096419d9989a73995bf9c9e08aca
Kevin Ansfield <kev@digitalblueprint.co.uk>
2007-12-03 10:06:15 +0000

    Improved error messages for avatar uploads
    git-svn-id: svn://svn.digitalblueprint.co.uk/viper/trunk@141 31e3d8c1-9a35-0410-80f7-bb7063ef511c

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -12,17 +12,7 @@ class Avatar < ActiveRecord::Base
                                    :micro => '25x25'},
                   :processor => 'rmagick'
   
-  validates_presence_of :size, :content_type, :filename
+  validates_attachment :content_type => "The file you uploaded was not a JPEG, PNG or GIF",
+                       :size         => "The image you uploaded was larger than the maximum size of 500KB" 
   
-  # validates the size and content_type attributes according to the current model's options
-  def validate
-    [:content_type].each do |attr_name|
-      enum = attachment_options[attr_name]
-      errors.add attr_name, 'is not a valid image type' unless enum.nil? || enum.include?(send(attr_name))
-    end
-    [:size].each do |attr_name|
-      enum = attachment_options[attr_name]
-      errors.add attr_name, 'is not in our acceptable range, image size should be 5KB to 500KB' unless enum.nil? || enum.include?(send(attr_name))
-    end
-  end
 end
---
+++ vendor/plugins/attachment_fu_validates_attachment/init.rb
@@ -0,0 +1,31 @@
+Technoweenie::AttachmentFu::InstanceMethods.module_eval do
+  protected
+    def attachment_valid?
+      if self.filename.nil?
+        errors.add_to_base attachment_validation_options[:empty]
+        return
+      end
+      [:content_type, :size].each do |option|
+        if attachment_validation_options[option] && attachment_options[option] && !attachment_options[option].include?(self.send(option))
+          errors.add_to_base attachment_validation_options[option]
+        end
+      end
+    end
+end
+
+Technoweenie::AttachmentFu::ClassMethods.module_eval do
+  # Options: 
+  # *  <tt>:empty</tt> - Base error message when no file is uploaded. Default is "No file uploaded" 
+  # *  <tt>:content_type</tt> - Base error message when the uploaded file is not a valid content type.
+  # *  <tt>:size</tt> - Base error message when the uploaded file is not a valid size.
+  #
+  # Example:
+  #   validates_attachment :content_type => "The file you uploaded was not a JPEG, PNG or GIF",
+  #                        :size         => "The image you uploaded was larger than the maximum size of 10MB" 
+  def validates_attachment(options={})
+    options[:empty] ||= "No file uploaded" 
+    class_inheritable_accessor :attachment_validation_options
+    self.attachment_validation_options = options
+    validate :attachment_valid?
+  end
+end
