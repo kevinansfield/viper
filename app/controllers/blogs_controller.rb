e865bcaefc484de2224ca0dbad5651eb1a4d72a5
Kevin Ansfield <kev@digitalblueprint.co.uk>
2007-12-03 13:04:04 +0000

    Added rss feeds for all blogs and individual user blogs. Closes #41 [#41 state:resolved]
    git-svn-id: svn://svn.digitalblueprint.co.uk/viper/trunk@143 31e3d8c1-9a35-0410-80f7-bb7063ef511c

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,15 @@ class BlogsController < ApplicationController
     self.maincol_two = nil
     self.sidebar_one = nil
     
-    @posts = Post.find_latest_by_unique_authors(10)
+    respond_to do |format|
+      format.html do
+        @posts = Post.find_latest_by_unique_authors(10)
+      end
+      format.rss do
+        @posts = Post.find_latest(25)
+        render :action => 'index.rxml', :layout => false
+      end
+    end
   end
   
   def show
