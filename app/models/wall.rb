d0c256c9bf21dc8751dd0513ed9cb237ce9a4f17
Kevin Ansfield <kev@digitalblueprint.co.uk>
2007-12-03 14:54:34 +0000

    Make sure related comments get destroyed if a wall is ever removed
    git-svn-id: svn://svn.digitalblueprint.co.uk/viper/trunk@153 31e3d8c1-9a35-0410-80f7-bb7063ef511c

--- app/models/wall.rb
+++ app/models/wall.rb
@@ -1,4 +1,4 @@
 class Wall < ActiveRecord::Base
   belongs_to :user
-  has_many :comments, :as => :commentable
+  has_many :comments, :as => :commentable, :order => 'created_at', :dependent => :destroy
 end
