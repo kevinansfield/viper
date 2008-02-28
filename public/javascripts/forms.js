4e237c477e737f90cce07bb8598a78a5cf657838
Kevin Ansfield <kev@digitalblueprint.co.uk>
2008-02-28 13:32:01 +0000

    Add forms.js and javascript resizing code

---
+++ public/javascripts/forms.js
@@ -0,0 +1,17 @@
+var forms = {
+	resizeTextArea: function(textarea, collapsed) {
+		var lines = textarea.value.split("\n");
+	    var count = lines.length;
+	    lines.each(function(line) { count += parseInt(line.length / 70); });
+	
+	    var rows = parseInt(collapsed / 20);
+	
+	    if (count > rows) {
+			textarea.style.height = (collapsed * 2) + 'px';
+	    }
+	
+	    if (count <= rows) {
+			textarea.style.height = collapsed + 'px';
+	    }
+	}
+}
