96568168b86b5846c877a62d52f3578bd46aecd7
Kevin Ansfield <kev@digitalblueprint.co.uk>
2008-02-29 14:00:02 +0000

    Switch bio questions to use TagLists and format accordingly in views

--- app/models/bio.rb
+++ app/models/bio.rb
@@ -8,10 +8,34 @@ class Bio < ActiveRecord::Base
   FAVORITES = QUESTIONS - %w(about author_about)
   
   acts_as_ferret :remote => false
-  acts_as_textiled :about, :interests, :music, :films, :television, :books, :heroes
+  acts_as_textiled :about
   
   validates_length_of QUESTIONS, :maximum => 65000
   validates_length_of :author_about, :maximum => 1000
+  
+  def interests
+    TagList.new(self[:interests], :parse => true)
+  end
+  
+  def music
+    TagList.new(self[:music], :parse => true)
+  end
+  
+  def films
+    TagList.new(self[:films], :parse => true)
+  end
+  
+  def television
+    TagList.new(self[:television], :parse => true)
+  end
+  
+  def books
+    TagList.new(self[:books], :parse => true)
+  end
+  
+  def heroes
+    TagList.new(self[:heroes], :parse => true)
+  end
                        
   private
   
