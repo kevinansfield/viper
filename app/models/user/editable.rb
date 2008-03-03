99ef1b217c1115d85e2121c0e4b9c8d813efcf34
Kevin Ansfield <kev@digitalblueprint.co.uk>
2008-03-03 17:15:36 +0000

    Expand User::Editable to work with multiple model types

--- app/models/user/editable.rb
+++ app/models/user/editable.rb
@@ -1,5 +1,10 @@
 module User::Editable
   def editable_by?(user)
-    user && (user.id == user_id || user.moderator_of?(forum))
+    case self.class.to_s
+      when 'Forum', 'ForumTopic', 'ForumPost'
+        return user && (user.id == user_id || user.moderator_of?(forum))
+      else
+        return user && user.id == user_id
+    end
   end
 end
