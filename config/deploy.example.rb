eb630ac26a32275de7a152927ca6a7a1de6d05d4
Kevin Ansfield <kev@digitalblueprint.co.uk>
2008-06-23 09:12:01 +0100

    Update deployment example

--- config/deploy.example.rb
+++ config/deploy.example.rb
@@ -1,43 +1,62 @@
 set :application, "viper.digitalblueprint.co.uk"
-set :repository,  "git@github.com:kevinansfield/viper.git"
-
+set :repository, "git@github.com:kevinansfield/viper.git"
+ 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 set :deploy_to, "/home/digitalb/public_html/#{application}"
-
+ 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 set :scm, :git
-# set :branch, "stable"
+#set :branch, "website_branch"
 set :repository_cache, "git_cache"
-
+ 
 role :app, "viper.digitalblueprint.co.uk"
 role :web, "viper.digitalblueprint.co.uk"
-role :db,  "viper.digitalblueprint.co.uk", :primary => true
-
+role :db, "viper.digitalblueprint.co.uk", :primary => true
+ 
 set :rails_env, "production"
-
+ 
 set :use_sudo, true
 set :user, "digitalb"
 set :jail_group, "www-data"
+ 
+set :runner, "digitalb"
+set :admin_runner, "digitalb"
 
-set :runner, nil
-
-task :restart do
-  sudo "/usr/local/lsws/bin/lswsctrl restart"
+# mod_rails restart
+deploy.task :start, :roles => :app do
+  run "touch #{current_path}/tmp/restart.txt"
+end
+ 
+deploy.task :restart, :roles => :app do
+  run "touch #{current_path}/tmp/restart.txt"
 end
 
-task :before_symlink do
+# litespeed restart
+#deploy.task :start, :roles => :app do
+#  sudo "/usr/local/bin/lsws/bin/lswsctrl restart"
+#end
+# 
+#deploy.task :restart, :roles => :app do
+#  sudo "/usr/local/bin/lsws/bin/lswsctrl restart"
+#end
+
+ 
+task :before_symlink, :roles => :app do
   #link in shared
-  sudo "ln -s #{deploy_to}/#{shared_dir}/avatars #{current_release}/public/avatars"
-  sudo "ln -s #{deploy_to}/#{shared_dir}/index #{current_release}/public/index"
+  run "ln -s #{deploy_to}/#{shared_dir}/avatars #{current_release}/public/avatars"
+  run "ln -s #{deploy_to}/#{shared_dir}/index #{current_release}/public/index"
   #link in config
-  sudo "cp #{deploy_to}/#{shared_dir}/system/database.yml #{current_release}/config/database.yml"
-  sudo "cp #{deploy_to}/#{shared_dir}/system/environment.rb #{current_release}/config/environment.rb"
-  sudo "cp #{deploy_to}/#{shared_dir}/system/constants.rb #{current_release}/config/initializers/constants.rb"
+  run "cp #{deploy_to}/#{shared_dir}/system/database.yml #{current_release}/config/database.yml"
+  run "cp #{deploy_to}/#{shared_dir}/system/environment.rb #{current_release}/config/environment.rb"
+  run "cp #{deploy_to}/#{shared_dir}/system/production.rb #{current_release}/config/environments/production.rb"
+  run "cp #{deploy_to}/#{shared_dir}/system/constants.rb #{current_release}/config/initializers/constants.rb"
+  #process theme cache
+  run "cd #{current_release}; rake theme_create_cache"
 end
-
-task :after_update_code do
+ 
+task :after_update_code, :roles => :app do
   sudo "chmod a+x #{release_path}/script/process/*"
 end
